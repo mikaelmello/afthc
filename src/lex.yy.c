5bfb65edc11394ebb2bfd4e5eb384904364798db
Mikael Mello <git@mikaelmello.com>
2019-09-08 22:40:56 -0300

    Add makefile and modifications to gitignore
    Signed-off-by: Mikael Mello <git@mikaelmello.com>

--- src/lex.yy.c
+++ src/lex.yy.c
@@ -351,8 +351,8 @@ static void yynoreturn yy_fatal_error ( const char* msg  );
 	(yy_hold_char) = *yy_cp; \
 	*yy_cp = '\0'; \
 	(yy_c_buf_p) = yy_cp;
-#define YY_NUM_RULES 5
-#define YY_END_OF_BUFFER 6
+#define YY_NUM_RULES 1
+#define YY_END_OF_BUFFER 2
 /* This struct is not used in this scanner,
    but its presence is necessary. */
 struct yy_trans_info
@@ -360,24 +360,23 @@ struct yy_trans_info
 	flex_int32_t yy_verify;
 	flex_int32_t yy_nxt;
 	};
-static const flex_int16_t yy_accept[17] =
+static const flex_int16_t yy_accept[6] =
     {   0,
-        0,    0,    6,    4,    5,    4,    1,    0,    3,    0,
-        0,    1,    0,    2,    0,    0
+        0,    0,    2,    1,    0
     } ;
 
 static const YY_CHAR yy_ec[256] =
     {   0,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    2,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    3,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    4,    1,    5,    5,    5,
-        5,    5,    5,    5,    5,    5,    5,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    6,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
 
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
@@ -398,37 +397,29 @@ static const YY_CHAR yy_ec[256] =
         1,    1,    1,    1,    1
     } ;
 
-static const YY_CHAR yy_meta[7] =
+static const YY_CHAR yy_meta[2] =
     {   0,
-        1,    2,    1,    1,    3,    1
+        1
     } ;
 
-static const flex_int16_t yy_base[21] =
+static const flex_int16_t yy_base[7] =
     {   0,
-        0,    0,    9,   34,   34,    4,    7,   10,   34,    0,
-        0,    0,   14,    0,   18,   34,   24,   27,    5,   30
+        0,    0,    2,    3,    3,    0
     } ;
 
-static const flex_int16_t yy_def[21] =
+static const flex_int16_t yy_def[7] =
     {   0,
-       16,    1,   16,   16,   16,   17,   16,   17,   16,   18,
-       19,    7,   20,   19,   20,    0,   16,   16,   16,   16
+        6,    6,    5,    5,    0,    5
     } ;
 
-static const flex_int16_t yy_nxt[41] =
+static const flex_int16_t yy_nxt[5] =
     {   0,
-        4,    5,    6,    4,    7,    4,    9,   14,   16,   10,
-       11,   12,    9,   16,   16,   10,    9,   16,   16,   10,
-        9,   16,   16,   10,    8,    8,    8,   13,   16,   13,
-       15,   15,   15,    3,   16,   16,   16,   16,   16,   16
+        4,    5,    3,    5
     } ;
 
-static const flex_int16_t yy_chk[41] =
+static const flex_int16_t yy_chk[5] =
     {   0,
-        1,    1,    1,    1,    1,    1,    6,   19,    3,    6,
-        7,    7,    8,    0,    0,    8,   13,    0,    0,   13,
-       15,    0,    0,   15,   17,   17,   17,   18,    0,   18,
-       20,   20,   20,   16,   16,   16,   16,   16,   16,   16
+        6,    3,    5,    5
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -448,9 +439,8 @@ char *yytext;
 #line 1 "tokenizer.lex"
 /* Scanner for Afth */
 #line 4 "tokenizer.lex"
-#include <math.h>
-#line 452 "lex.yy.c"
-#line 453 "lex.yy.c"
+#line 442 "lex.yy.c"
+#line 443 "lex.yy.c"
 
 #define INITIAL 0
 
@@ -670,7 +660,7 @@ YY_DECL
 #line 70 "tokenizer.lex"
 
 
-#line 673 "lex.yy.c"
+#line 663 "lex.yy.c"
 
 	while ( /*CONSTCOND*/1 )		/* loops until end-of-file is reached */
 		{
@@ -697,13 +687,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 17 )
+				if ( yy_current_state >= 6 )
 					yy_c = yy_meta[yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 34 );
+		while ( yy_base[yy_current_state] != 3 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -730,30 +720,9 @@ do_action:	/* This label is used only to access EOF actions. */
 case 1:
 YY_RULE_SETUP
 #line 72 "tokenizer.lex"
-printf( "An integer: %s\n", yytext );
-	YY_BREAK
-case 2:
-YY_RULE_SETUP
-#line 74 "tokenizer.lex"
-printf( "A real number: %s\n", yytext );
-	YY_BREAK
-case 3:
-/* rule 3 can match eol */
-YY_RULE_SETUP
-#line 76 "tokenizer.lex"
-printf( "A string: %s\n", yytext );
-	YY_BREAK
-case 4:
-YY_RULE_SETUP
-#line 78 "tokenizer.lex"
-printf( "Unrecognized character: %s\n", yytext );
-	YY_BREAK
-case 5:
-YY_RULE_SETUP
-#line 80 "tokenizer.lex"
 ECHO;
 	YY_BREAK
-#line 756 "lex.yy.c"
+#line 725 "lex.yy.c"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -1050,7 +1019,7 @@ static int yy_get_next_buffer (void)
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 17 )
+			if ( yy_current_state >= 6 )
 				yy_c = yy_meta[yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
@@ -1078,11 +1047,11 @@ static int yy_get_next_buffer (void)
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 17 )
+		if ( yy_current_state >= 6 )
 			yy_c = yy_meta[yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
-	yy_is_jam = (yy_current_state == 16);
+	yy_is_jam = (yy_current_state == 5);
 
 		return yy_is_jam ? 0 : yy_current_state;
 }
@@ -1758,7 +1727,7 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 80 "tokenizer.lex"
+#line 72 "tokenizer.lex"
 
 
 int main(int argc, char* argv[]) {
